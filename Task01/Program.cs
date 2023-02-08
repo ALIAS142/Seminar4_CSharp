0c1eaa85f9e2ad5ef6196e9dc415f573d7cbe150
Anna <kochurovaav@gmail.com>
2023-02-08 15:19:09 +0200

    Task01 is made

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+System.Console.Write("Enter number A :");
+int numA = int.Parse(Console.ReadLine());
+
+if (numA < 1)
+{
+    System.Console.WriteLine("Number is less then 1");
+}
+else{
+    int sum = 0;
+
+for (int numB = 1; numB <= numA; numB++)
+{
+    sum+= numB;
+}
+System.Console.WriteLine(sum);
+}
+
+
+    
