f2430007566ee5ab4c8797d3d72d0a82d765cea8
Anna <kochurovaav@gmail.com>
2023-02-08 16:01:23 +0200

    Task01 is changed

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -7,14 +7,25 @@ if (numA < 1)
 {
     System.Console.WriteLine("Number is less then 1");
 }
+
 else{
     int sum = 0;
-
-for (int numB = 1; numB <= numA; numB++)
+    for (int numB = 1; numB <= numA; numB++)
 {
     sum+= numB;
 }
 System.Console.WriteLine(sum);
+}
+
+
+int GetSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= numA; i++)
+{
+    sum+= i;
+}
+return sum;
 }
 
 
