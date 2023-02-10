6f3331f0d3c69685e466ce4caac211bc1775705b
Anna <kochurovaav@gmail.com>
2023-02-10 12:55:12 +0200

    Task02 is added

---
+++ Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Enter number A :");
+int Number = int.Parse(Console.ReadLine());
+
+if (Number <= 0)
+{
+    System.Console.WriteLine("Error");
+}
+
+else{
+System.Console.WriteLine($"countNumbers = {countNumbers(Number)}");
+}
+
+int countNumbers(int Num)
+{
+    int count = 0;
+    for(; Num > 0; count++)
+    {
+        Num = Num/10;
+    }
+    return count;
+
+}
